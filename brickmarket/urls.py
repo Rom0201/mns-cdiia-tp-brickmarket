dd3525c9325fe53a731fe57a48e5bf79578681a9
Nicolas Nunge <nicolas@nicolasnunge.net>
2026-08-17 09:35:08 +0200

    feat: add new layout and catalog

--- brickmarket/urls.py
+++ brickmarket/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from catalog.views import home, product
+from catalog.views import product
+from pages.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name="home_route"),
     path('product/', product),
     path('product/<int:id>/', product)
 ]
